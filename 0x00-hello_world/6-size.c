007101b65605c126906b0f8433a0d7d1f07150e1
stella-maris2 <wandoo2123@gmail.com>
2022-09-09 04:11:31 -0700

    Task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - The entry point 
+ * main - The entry point
  * No arguement
  *
  * Description: Prints the bytes for various data types to stdo
@@ -15,11 +15,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	
-	printf("Size of a char: %d byte(s)\n" , sizeof(a));
-	printf("Size of an int: %d byte(s)\n" , sizeof(b));
-	printf("Size of a long int: %d byte(s)\n" , sizeof(c));
-	printf("Size of a long long int: %d byte(s)\n" , sizeof(d));
-	printf("Size of a float: %d byte(s)\n" , sizeof(e));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
