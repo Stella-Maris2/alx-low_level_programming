b789f0afd52b332b496f3cd03f7b94ebaba828cf
Stella-Maris2 <wandoo2123@gmail.com>
2022-09-12 19:29:52 +0000

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,46 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - print single didgit numbers starting from 0
+ * main - Entry point
  *
- * Return: returns zero at the end
+ * Return: Always (Success)
  */
-
 int main(void)
-
 {
-	int i, j;
+	int digit1, digit2;
 
-	for (i = 0 ; i <= 9 ; i++)
-{
-
-		for (j = 1 ; j <= 9 ; j++)
+	for (digit1 = 0 ; digit1 < 9 ; digit1++)
+	{
+		for (digit2 = digit1 + 1 ; digit2 < 10 ; digit2++)
 		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-		if (j > i);
-		{
+			if (digit1 == 8 && digit2 == 9)
+			continue;
 
-	putchar(i + '0');
-
-	putchar(j + '0');
-
-
-if (i != 8)
-
-{
-
-putchar(',');
-
-putchar(' ');
-
+			putchar(',');
+			putchar(' ');
 }
-
 }
-
-}
-
-}
-
 putchar('\n');
 
 return (0);
