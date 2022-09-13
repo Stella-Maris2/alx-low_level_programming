55a60cbfa4a560bb8cab3632943de4f726b45631
Stella-Maris2 <wandoo2123@gmail.com>
2022-09-13 22:09:41 +0000

    Majesty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,6 @@ while (hours <= 23)
 {
 while (minutes <= 59)
 {
-
 mins_remainder = minutes % 10;
 hours_remainder = hours % 10;
 _putchar(hours / 10 + '0');
@@ -29,5 +28,6 @@ _putchar('\n');
 }
 hours++;
 minutes = 0;
+
 }
 }
