7b6aa562c0f638c5e434ea8f84aed35efdcd5136
Stella-Maris2 <wandoo2123@gmail.com>
2022-09-19 20:56:38 +0000

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,12 @@
 
 void print_rev(char *s)
 {
-	int len = strlen(s);
 
-	while (len--)
+int len = strlen(s);
 
-		putchar(*(s + len));
-	putchar (10);
+while (len--)
+
+putchar(*(s + len));
+
+putchar (10);
 }
