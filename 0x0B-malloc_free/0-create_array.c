73d3c0c2bd49fbc69783409d0ad3eedea61f9371
Stella-Maris2 <wandoo2123@gmail.com>
2022-10-04 14:38:44 +0000

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,22 +15,14 @@ char *arr;
 unsigned int i;
 
 if (size == 0)
-{
 return (NULL);
-}
-
 arr = malloc(sizeof(char) * size);
-
-/* check if malloc was successful */
 
 if (arr == NULL)
 	return (NULL);
 
 	for (i = 0 ; i < size ; i++)
-{
 	arr[i] = c;
-}
 
 return (arr);
-
 }
