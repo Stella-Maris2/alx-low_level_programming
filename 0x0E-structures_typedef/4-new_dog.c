a80bd1450fdc4b5b7143e67722ad917408a84831
Stella-Maris2 <wandoo2123@gmail.com>
2022-10-11 14:24:34 +0000

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,7 @@ char *_strcopy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- * _strlen: Finds the length of a string.
+ * _strlen - Finds the length of a string.
  * @str: The string to be measured.
  *
  * Return: The length of the string.
@@ -43,7 +43,7 @@ char *_strcopy(char *dest, char *src)
 }
 
 /**
- * new_dog: Creates a new dog.
+ * new_dog - Creates a new dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
